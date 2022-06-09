233678bc9d53072ff485b4181e075eea47e2b416
jhamin0511 <jhamin0511@gmail.com>
2022-06-09 16:38:16 +0900

    feat(Hilt): Annotation 적용

---
+++ app/src/main/java/com/github/jhamin0511/mystudy/application/StudyApplication.kt
@@ -0,0 +1,7 @@
+package com.github.jhamin0511.mystudy.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StudyApplication : Application()
